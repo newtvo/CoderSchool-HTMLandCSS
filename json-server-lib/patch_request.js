cf9359197da9feed72030c63147ca7cad2a69a28
Nhut Vo <nhutmvo1997@gmail.com>
2020-09-04 23:59:43 -0400

    patch request

--- json-server-lib/patch_request.js
+++ json-server-lib/patch_request.js
@@ -3,7 +3,7 @@ var axios = require('axios');
 axios.patch('http://localhost:3000/users/4/', {
     first_name: "Robert",
     last_name: "Vo",
-    email: "bobbrown432@aol.com"
+    email: "bobbrown432@zing.com"
   })
   .then(res => {
       console.log(res.data);
